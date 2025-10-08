febda70fe1dd8d869e211f1bc33e37540a4ff23c
aremo90 <mrhamza2005@gmail.com>
2025-10-08 13:47:30 +0300

    Models Created

---
+++ GymSystemDAL/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Models
+{
+    internal class Member : GymUser
+    {
+        // CreatedAd will be inherited from BaseEntity and it name will be altered to JoinDate
+        public string? Photo { get; set; }
+    }
+}
---
+++ GymSystemDAL/Models/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using GymSystemDAL.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Models
+{
+    internal class Trainer : GymUser
+    {
+        // CreatedAd will be inherited from BaseEntity and it name will be altered to HireDate
+        public Specialites Specialites { get; set; }
+
+    }
+}
