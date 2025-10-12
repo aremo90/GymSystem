14937a332bdc9db2207070161fb1965e3b6b4c99
aremo90 <mrhamza2005@gmail.com>
2025-10-12 23:11:02 +0300

    Repository Design Pattern

---
+++ GymSystemDAL/Data/Repositroies/Interfaces/IMemberRepository.cs
@@ -0,0 +1,27 @@
+﻿using GymSystemDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Data.Repositroies.Interfaces
+{
+    internal interface IMemberRepository
+    {
+        //Get all Members
+        IEnumerable<Member> GetAll();
+
+        // Get Member by ID
+        Member? GetById(int id);
+
+        // Add a new Member
+        int Add(Member member);
+
+        // delete a Member
+        int Delete(int id);
+
+        // Update a Member
+        int Update(Member member);
+    }
+}
